0ff493752164114e4a93e3431bb3218ffd433d8c
Hermawan Wijaya <hermawanwijaya21@gmail.com>
2020-05-17 18:04:24 +0700

    Add navigation

--- assets/js/goquiz/get_index.js
+++ assets/js/goquiz/get_index.js
@@ -6,4 +6,9 @@ $(document).ready(function() {
     $('.menu-wrapper #btn-quiz').click(function() {
         window.location.href = '/goquiz/quiz';
     });
+
+    $('.menu-wrapper #btn-quit').click(function() {
+        console.log("quit")
+        window.location.href = '/';
+    });
 });
---
+++ assets/js/home/get_index.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('.dropdown-menu #go-quiz').click(function() {
+        window.location.href = '/goquiz';
+    });
+});
