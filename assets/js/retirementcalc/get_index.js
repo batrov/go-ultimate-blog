d16c8f7c6ad1562ee14f823114daf1d5b2bcbbf0
Hermawan Wijaya <hermawanwijaya21@gmail.com>
2020-06-01 11:42:15 +0700

    Add yearly raise

--- assets/js/retirementcalc/get_index.js
+++ assets/js/retirementcalc/get_index.js
@@ -5,6 +5,20 @@ $(document).ready(function() {
         doCalculate()
     });
 
+    $('.menu-wrapper #btn-advanced').click(function() {
+        advanced = $("#advanced").val()
+        if (advanced == "0") {
+            $("#advanced").val("1")
+            $('#btn-advanced').removeClass("btn-warning")
+            $('#btn-advanced').addClass("btn-info")
+            $('#btn-advanced').text("Advanced Mode: True")
+        } else {
+            $("#advanced").val("0")
+            $('#btn-advanced').removeClass("btn-info")
+            $('#btn-advanced').addClass("btn-warning")
+            $('#btn-advanced').text("Advanced Mode: False")
+        }
+    });
 
     $('.menu-wrapper #btn-exit').click(function() {
         console.log("exit")
@@ -20,24 +34,32 @@ $(document).ready(function() {
         expenses = $("#expenses").val()
         inflation = $("#inflation").val()
         currency = $("#currency").val()
+        raise = $("#raise").val()
 
         workingYear = lifespan - age
         yearlyExpenses = expenses * 12
+        raisedIncome = income
         lifetimeExpenses = 0
+        lifetimeIncome = 0
+        workingMonth = 0
 
         for (i = 0; i < workingYear; i++) {
             yearlyExpenses = yearlyExpenses + (inflation / 100 * yearlyExpenses)
             lifetimeExpenses += yearlyExpenses
         }
 
-        if (income != 0) {
-            estimatedWorkPeriod = Math.ceil(lifetimeExpenses / income)
-        } else {
-            estimatedWorkPeriod = workingYear * 12
+        for (i = 0; i < workingYear * 12; i++) {
+            workingMonth++
+            raisedIncome = raisedIncome * (100 + (raise / 12)) / 100
+            lifetimeIncome += raisedIncome
+
+            if (lifetimeIncome >= lifetimeExpenses) {
+                break
+            }
         }
 
-        yearOfWork = Math.floor(estimatedWorkPeriod / 12) + " year"
-        monthOfWork = estimatedWorkPeriod % 12 + " month"
+        yearOfWork = Math.floor(workingMonth / 12) + " year"
+        monthOfWork = workingMonth % 12 + " month"
 
         result = `You have to work for ${yearOfWork + " " + monthOfWork} to cover your ${lifetimeExpenses.toLocaleString()} ${currency} lifetime expenses.`
         $('#result').text(result)
@@ -49,8 +71,6 @@ $(document).ready(function() {
             resultRetire = `Retired at age ${parseInt(age) + parseInt(yearOfWork)}`
             $('#result-retire').removeClass("text-danger")
             $('#result-retire').addClass("text-success")
-
-            lifetimeIncome = estimatedWorkPeriod * income
 
             savings = lifetimeIncome - lifetimeExpenses
 
