aba339aeef4ceb3361b70c79b646a067fcd020b7
Hermawan Wijaya <hermawanwijaya21@gmail.com>
2020-09-11 23:07:59 +0700

    Use context

--- commons/functions.go
+++ commons/functions.go
@@ -7,10 +7,15 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+var ctx = context.Background()
+
+func GetContext() context.Context {
+	return ctx
+}
 
 func GetPort() string {
 	p := os.Getenv("PORT")
@@ -46,22 +51,24 @@ func GetTemplate() *template.Template {
 func Middleware(next httprouter.Handle) httprouter.Handle {
 
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
-		defer cancel()
+		ctxKey := fmt.Sprintf("is-https-%s%s", r.Host, r.URL.Path)
 
-		if GetEnv() == "production" && ctx.Value("is-https") != "1" {
+		if GetEnv() == "production" && ctx.Value(ctxKey) != "1" {
 			target := "https://" + r.Host + r.URL.Path
 			if len(r.URL.RawQuery) > 0 {
 				target += "?" + r.URL.RawQuery
 			}
 			log.Printf("redirect to: %s", target)
 
-			ctx = context.WithValue(ctx, "is-https", "1")
+			ctx = context.WithValue(ctx, ctxKey, "1")
+			log.Println("ctx set")
+
 			r = r.WithContext(ctx)
 
 			http.Redirect(w, r, target, http.StatusTemporaryRedirect)
 			return
 		} else {
+			ctx = context.WithValue(ctx, ctxKey, "0")
 			next(w, r, ps)
 		}
 
