5343b7ea0d0fa24f4060991503106f09304678b2
Hermawan Wijaya <hermawanwijaya21@gmail.com>
2020-09-12 10:28:05 +0700

    Complete profile

--- assets/vendor/templatemo-ultraprofile/js/custom.js
+++ assets/vendor/templatemo-ultraprofile/js/custom.js
@@ -1,92 +1,91 @@
-
-
 // ISOTOPE FILTER
 
-jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
 
-	if ( $('.iso-box-wrapper').length > 0 ) { 
+    if ($('.iso-box-wrapper').length > 0) {
 
-	    var $container 	= $('.iso-box-wrapper'), 
-	    	$imgs 		= $('.iso-box img');
+        var $container = $('.iso-box-wrapper'),
+            $imgs = $('.iso-box img');
 
 
 
-	    $container.imagesLoaded(function () {
+        $container.imagesLoaded(function() {
 
-	    	$container.isotope({
-				layoutMode: 'fitRows',
-				itemSelector: '.iso-box'
-	    	});
+            $container.isotope({
+                layoutMode: 'fitRows',
+                itemSelector: '.iso-box'
+            });
 
-	    	$imgs.load(function(){
-	    		$container.isotope('reLayout');
-	    	})
+            $imgs.load(function() {
+                $container.isotope('reLayout');
+            })
 
-	    });
+        });
 
-	    //filter items on button click
+        //filter items on button click
 
-	    $('.filter-wrapper li a').click(function(){
+        $('.filter-wrapper li a').click(function() {
 
-	        var $this = $(this), filterValue = $this.attr('data-filter');
+            var $this = $(this),
+                filterValue = $this.attr('data-filter');
 
-			$container.isotope({ 
-				filter: filterValue,
-				animationOptions: { 
-				    duration: 750, 
-				    easing: 'linear', 
-				    queue: false, 
-				}              	 
-			});	            
+            $container.isotope({
+                filter: filterValue,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false,
+                }
+            });
 
-			// don't proceed if already selected 
+            // don't proceed if already selected 
 
-			if ( $this.hasClass('selected') ) { 
-				return false; 
-			}
+            if ($this.hasClass('selected')) {
+                return false;
+            }
 
-			var filter_wrapper = $this.closest('.filter-wrapper');
-			filter_wrapper.find('.selected').removeClass('selected');
-			$this.addClass('selected');
+            var filter_wrapper = $this.closest('.filter-wrapper');
+            filter_wrapper.find('.selected').removeClass('selected');
+            $this.addClass('selected');
 
-	      return false;
-	    }); 
+            return false;
+        });
 
-	}
+    }
 
 });
 
 
 // MAIN NAVIGATION
 
- $('.main-navigation').onePageNav({
-        scrollThreshold: 0.2, // Adjust if Navigation highlights too early or too late
-        scrollOffset: 75, //Height of Navigation Bar
-        filter: ':not(.external)',
-        changeHash: true
-    }); 
+$('.main-navigation').onePageNav({
+    scrollThreshold: 0.2, // Adjust if Navigation highlights too early or too late
+    scrollOffset: 75, //Height of Navigation Bar
+    filter: ':not(.external)',
+    changeHash: true
+});
 
-    /* NAVIGATION VISIBLE ON SCROLL */
+/* NAVIGATION VISIBLE ON SCROLL */
+mainNav();
+$(window).scroll(function() {
     mainNav();
-    $(window).scroll(function () {
-        mainNav();
-    });
+});
 
-    function mainNav() {
-        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-        if (top > 40) $('.sticky-navigation').stop().animate({
-            "opacity": '1',
-            "top": '0'
-        });
-        else $('.sticky-navigation').stop().animate({
-            "opacity": '0',
-            "top": '-75'
-        });
-    }
+function mainNav() {
+    var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+    if (top > 40) $('.sticky-navigation').stop().animate({
+        "opacity": '1',
+        "top": '0'
+    });
+    else $('.sticky-navigation').stop().animate({
+        "opacity": '0',
+        "top": '-75'
+    });
+}
 
 
 // HIDE MOBILE MENU AFTER CLIKING ON A LINK
 
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
+$('.navbar-collapse a').click(function() {
+    $(".navbar-collapse").collapse('hide');
+});
