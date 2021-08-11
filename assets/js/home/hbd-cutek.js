19f31258514cf1b73ee1ef57054eb35f940587b1
Hermawan Wijaya <hermawan@ruparupa.com>
2021-08-11 10:13:52 +0700

    Add ucapan animation

--- assets/js/home/hbd-cutek.js
+++ assets/js/home/hbd-cutek.js
@@ -16,23 +16,36 @@ $(document).ready(function() {
 
 function runningUcapan(audioSec) {
     var isFinished = false;
-    if (audioSec > 15) {
+    if (audioSec > 26) {
+        clearAnimation();
+        $('.ucapan').addClass("w3-animate-top");
+        $('.ucapan').text("Nih foto-foto Molly :D")
+        $('.ucapan-img').show();
+    } if (audioSec > 25) {
+        clearAnimation();
+        $('.ucapan').addClass("w3-animate-top");
+        $('.ucapan').text("Nih foto-foto Molly :D")
+    } else if (audioSec > 20) {
+        clearAnimation();
+        $('.ucapan').addClass("w3-animate-top");
+        $('.ucapan').text("Supaya gak bad mood ...")
+    } else if (audioSec > 15) {
         clearAnimation();
         $('.ucapan').addClass("w3-animate-right");
         $('.ucapan').text("Semoga baik-baik aja yak :)")
-        isFinished = true;
     } else if (audioSec > 10) {
         clearAnimation();
         $('.ucapan').addClass("w3-animate-left");
         $('.ucapan').text("Apa kabar kamu hari ini?")
-        isFinished = true;
     } else if (audioSec > 5) {
         clearAnimation();
         $('.ucapan').addClass("w3-animate-bottom");
         $('.ucapan').text("Hai Cutekk!")
     } else if (audioSec > 0) {
         clearAnimation();
+        // $('.audio').hide();
         $('.ucapan').addClass("w3-animate-top");
+        $('body').addClass("pantone");
         $('.ucapan').text("Nah gitu dong di play musiknya :D")
     }
 
@@ -44,4 +57,10 @@ function clearAnimation() {
     $('.ucapan').removeClass("w3-animate-top");
     $('.ucapan').removeClass("w3-animate-left");
     $('.ucapan').removeClass("w3-animate-right");
+
+    $('.ucapan-img').removeClass("w3-animate-bottom");
+    $('.ucapan-img').removeClass("w3-animate-top");
+    $('.ucapan-img').removeClass("w3-animate-left");
+    $('.ucapan-img').removeClass("w3-animate-right");
+
 }
